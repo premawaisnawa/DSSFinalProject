f5ebda25cae4974090036313ece849fd09ea614a
PREMAWAISNAWA <premawaisnawa@gmail.com>
2017-12-01 20:10:30 +0800

    Penambahan activity Daftar Calon ketua dan penambah widget beserta perubahan id

---
+++ app/src/main/java/com/example/premawaisnawa/dssfinalproject/DaftarCalonKetua.java
@@ -0,0 +1,13 @@
+package com.example.premawaisnawa.dssfinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DaftarCalonKetua extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daftar_calon_ketua);
+    }
+}
